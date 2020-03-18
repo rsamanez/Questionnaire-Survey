710768e730157cea02ce008b4de06b12c0aff90c
Rommel Samamnez <rsamanez@gmail.com>
2020-03-18 13:13:22 -0500

    changing welcome app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Questionnaires & Surveys
+                    {{ e(config('app.name', 'Laravel')) }}
                 </div>
 
 
